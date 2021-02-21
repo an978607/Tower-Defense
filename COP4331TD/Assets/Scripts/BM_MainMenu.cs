f9e913ee761bad5adca0ab901112e054b5985b8a
Dalton Kajander <daltonkajander@yahoo.com>
2021-02-20 19:06:16 -0500

    main menu music with volume slider

---
+++ COP4331TD/Assets/Scripts/BM_MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BM_MainMenu : MonoBehaviour {
+    //public GameObject playButton, settingsButton;
+    public Slider volumeSlider;
+    public AudioSource music;
+
+
+    //private bool settingsOpen = false;
+    void Start() {
+        music.volume = 0.5f;
+    }
+
+    public void playButtonPressed() {
+        SceneManager.LoadScene("Map00a");
+    }
+
+    public void settingsButtonPressed() {
+        // TODO
+        // create new GUI
+    }
+
+    public void changeMusicVolume() {
+        music.volume = volumeSlider.value;
+    }
+
+}
