e79fbda9d9ed772a6ea84bb2a77ea612091342a8
Dalton <daltonkajander@yahoo.com>
2021-02-28 17:48:08 -0500

    Patch 0.2.0 changes

---
+++ COP4331TD/Assets/Scripts/detectCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class detectCollision : MonoBehaviour {
+
+    public Text lives;
+    public GameObject level;
+    public GameObject spawner;
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Enemy") {
+            level.GetComponent<startLevel>().loseLives(1);
+            Destroy(collision.gameObject);
+            spawner.GetComponent<Spawner>().destroyEnemy();
+        }
+    }
+}
