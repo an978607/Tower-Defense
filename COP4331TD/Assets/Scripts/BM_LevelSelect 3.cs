b2868ceb0209935d8429240f7c2cbe7df548b658
Kevin Ramos <Kevramos93@yahoo.com>
2021-03-06 15:06:08 -0500

    meta stuff
    duplicates overwriting

---
+++ COP4331TD/Assets/Scripts/BM_LevelSelect 3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BM_LevelSelect : MonoBehaviour
+{
+ 
+    public void levelOneButtonPressed()
+    {
+        SceneManager.LoadScene("Map01");
+    }
+
+    public void levelTwoButtonPressed()
+    {
+        SceneManager.LoadScene("Map02");
+    }
+
+    public void levelThreeButtonPressed()
+    {
+        SceneManager.LoadScene("Map03");
+    }
+}
---
+++ COP4331TD/Assets/Scripts/BM_Prep 3.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BM_Prep : MonoBehaviour
+{
+ public void BackButtonPressed()
+    {
+        SceneManager.LoadScene("LevelSelection");
+    }
+
+    public void StartButtonPressed()
+    {
+        
+    }
+}
