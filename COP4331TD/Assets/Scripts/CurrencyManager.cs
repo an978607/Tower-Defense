3c37a9cc734fb36d2ab949d2986b292b9088d5fa
Kevin Ramos <Kevramos93@yahoo.com>
2021-03-06 16:30:35 -0500

    please change

---
+++ COP4331TD/Assets/Scripts/CurrencyManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyManager : MonoBehaviour
+{
+    public Text currencyDisplay;
+    public static int currentMoney;
+    public static bool quit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("CurrentMoney"))
+        {
+            currentMoney = PlayerPrefs.GetInt("CurrentMoney");
+        }
+        else
+        {
+            currentMoney = 500;
+            PlayerPrefs.SetInt("CurrentMoney", currentMoney);
+        }
+
+         currencyDisplay.text = currentMoney.ToString();
+     }
+
+// Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void AddCurrency(int amount)
+    {
+        currentMoney += amount;
+        PlayerPrefs.SetInt("CurrentMoney", currentMoney);
+        // currencyDisplay.text = currentMoney.ToString();
+    }
+
+    public void quitLevel()
+    {
+        quit = true;
+        updateCurrency();
+    }
+
+    private void updateCurrency()
+    {
+        if (quit != true)
+            return;
+        currentMoney -= 100;
+        if (currentMoney <= 0)
+            AddCurrency(500);
+        PlayerPrefs.SetInt("CurrentMoney", currentMoney);
+        quit = false;
+    }
+}
+
