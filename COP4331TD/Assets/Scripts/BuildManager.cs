c8a380079a8b1b983b630d8e5286b5f1d89b7251
an978607 <anneb17@knights.ucf.edu>
2021-03-14 17:08:06 -0400

    Updates to Shop
    Weapons can be selected and rendered on Map

---
+++ COP4331TD/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.Log("More then one buildMannager");
+        }
+        instance = this;
+    }
+
+    public GameObject weapon1Prefab;
+    public GameObject weapon2Prefab;
+    public GameObject weapon3Prefab;
+
+    private GameObject weaponToBuild;
+
+
+    public GameObject getWeaponToBuild()
+    {
+        return weaponToBuild;
+    }
+
+    public void setWeaponToBuild(GameObject weapon)
+    {
+        weaponToBuild = weapon;
+    }
+
+   
+}
---
+++ COP4331TD/Assets/Scripts/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject weapon; 
+
+    private Renderer rend;
+    private Color startColor;
+    BuildManager buildManager;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+        buildManager = BuildManager.instance;
+    }
+
+    private void OnMouseDown()
+    {
+        if(buildManager.getWeaponToBuild() == null)
+        {
+            return;
+        }
+        if(weapon != null)
+        {
+            //can't build because something is already on this node. 
+            return;
+        }
+
+        //build weapon
+        GameObject weaponToBuild = BuildManager.instance.getWeaponToBuild();
+        weapon = (GameObject)Instantiate(weaponToBuild, transform.position + positionOffset, transform.rotation);
+    }
+    void OnMouseEnter()
+    {
+        if (buildManager.getWeaponToBuild() == null)
+        {
+            return;
+        }
+        rend.material.color = hoverColor;
+    }
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+
+    }
+}
