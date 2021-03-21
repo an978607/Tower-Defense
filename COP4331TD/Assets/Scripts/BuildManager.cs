1770a3b62b73ac5a8a0341ce6564e593d23f4389
Kevin Ramos <Kevramos93@yahoo.com>
2021-03-21 13:22:00 -0400

    Revert "Merge branch 'main' of https://github.com/an978607/Tower-Defense into main"
    This reverts commit fd9b3858edbfeaccaa717bf10fbc09595f7f1853, reversing
    changes made to 5d481b0c195e0d4c500f6c2875ac746a21e0ad64.

--- COP4331TD/Assets/Scripts/BuildManager.cs
+++ COP4331TD/Assets/Scripts/BuildManager.cs
@@ -8,7 +8,7 @@ public class BuildManager : MonoBehaviour
 
     private void Awake()
     {
-        if (instance != null)
+        if(instance != null)
         {
             Debug.Log("More then one buildMannager");
         }
@@ -19,33 +19,18 @@ public class BuildManager : MonoBehaviour
     public GameObject weapon2Prefab;
     public GameObject weapon3Prefab;
 
-    public GameObject FundsDialoge;
-
-    private Turrent_BluePrint weaponToBuild;
+    private GameObject weaponToBuild;
 
 
-    //Node is empty and can be built on 
-    public bool CanBuild { get { return weaponToBuild != null; } }
-    //Does the player have enough 
-    public bool HasMoney { get { return CurrencyManager.currentBalance >= weaponToBuild.cost; } }
-
-    public void BuildWeaponOn(Node node)
+    public GameObject getWeaponToBuild()
     {
-        if (CurrencyManager.currentBalance < weaponToBuild.cost)
-        {
-            FundsDialoge.SetActive(true);
-            return;
-        }
-
-        //purchase upon build 
-        CurrencyManager.currentBalance -= weaponToBuild.cost;
-        GameObject weapon = (GameObject)Instantiate(weaponToBuild.prefab, node.getBuildPosition(), Quaternion.identity);
-        node.weapon = weapon;
+        return weaponToBuild;
     }
 
-    public void selectWeaponToBuild(Turrent_BluePrint weapon)
+    public void setWeaponToBuild(GameObject weapon)
     {
         weaponToBuild = weapon;
     }
 
+   
 }
